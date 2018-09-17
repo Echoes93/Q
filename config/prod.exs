9e351282e6d7fb5d1fa971fa48f2b9417e805f95
Dmitry Isachenko <isdmitry93@gmail.com>
2018-09-17 08:30:06 +0300

    prod config update

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ use Mix.Config
 config :q, QWeb.Endpoint,
   load_from_system_env: true,
   url: [scheme: "https", host: "q-phx.server.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]]
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
